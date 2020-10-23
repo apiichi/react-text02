491653829235310a99ab994146f1a74617163e44
Minji Kim <apiichi21@gmail.com>
2020-10-23 16:08:14 +0900

    Updates

---
+++ precache-manifest.02ee8525c6885bb4e8e8b7609a84898c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1e36af60deeb9ff61ccc05b306fe7e7",
+    "url": "/react-text02/index.html"
+  },
+  {
+    "revision": "e434be6fd7c220220409",
+    "url": "/react-text02/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "22ba895399dd657c67d8",
+    "url": "/react-text02/static/js/2.19eab2f7.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/react-text02/static/js/2.19eab2f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e434be6fd7c220220409",
+    "url": "/react-text02/static/js/main.4366ac41.chunk.js"
+  },
+  {
+    "revision": "e84ba1115d17e91dc072",
+    "url": "/react-text02/static/js/runtime-main.a633e68b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-text02/static/media/logo.5d5d9eef.svg"
+  }
+]);
